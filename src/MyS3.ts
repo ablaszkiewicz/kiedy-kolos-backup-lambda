47c678b7b112293ab8368aac6d8f5d3f272f0d86
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-10-07 21:14:54 +0200

    Initial commit

---
+++ src/MyS3.ts
@@ -0,0 +1,26 @@
+import { S3, SharedIniFileCredentials } from 'aws-sdk';
+import { WriteStream } from 's3-streams';
+
+export class MyS3 {
+  private s3!: S3;
+
+  constructor() {
+    this.initializeS3Connection();
+  }
+
+  private initializeS3Connection(): void {
+    const isLambda = process.env.LAMBDA_TASK_ROOT;
+
+    this.s3 = new S3({
+      region: 'eu-west-1',
+      ...(!isLambda ? { credentials: new SharedIniFileCredentials() } : {}),
+    });
+  }
+
+  public getWriteStream(key: string) {
+    return new WriteStream(this.s3, {
+      Bucket: process.env.AWS_BUCKET!,
+      Key: key,
+    });
+  }
+}
---
+++ src/handler.ts
@@ -0,0 +1,48 @@
+import { createReadStream } from 'fs';
+import mysqldump from 'mysqldump';
+import { MyS3 } from './MyS3';
+import dayjs from 'dayjs';
+import { createGzip } from 'zlib';
+require('dotenv').config();
+
+const MAX_RETRIES = 10;
+
+export const main = async () => {
+  const isLocalEnv = !process.env.LAMBDA_TASK_ROOT;
+  const dumpPath = isLocalEnv ? 'dump.sql' : '/tmp/dump.sql';
+
+  for (let i = 0; i < MAX_RETRIES; i++) {
+    try {
+      await mysqldump({
+        connection: {
+          host: process.env.DB_HOST!,
+          user: process.env.DB_USERNAME!,
+          port: +process.env.DB_PORT!,
+          password: process.env.DB_PASSWORD!,
+          database: process.env.DB_DATABASE!,
+        },
+        dumpToFile: dumpPath,
+      });
+      break;
+    } catch (e) {
+      continue;
+    }
+  }
+
+  console.log('Dumped to file');
+  const s3 = new MyS3();
+
+  const stream = createReadStream(dumpPath);
+  const key = dayjs().format('YYYY-MM-DD HH-mm-ss');
+  const writeStream = s3.getWriteStream(`${key}.sql.gz`);
+
+  stream.pipe(createGzip()).pipe(writeStream);
+  console.log('Opened S3 write stream');
+
+  const end = new Promise((resolve) => {
+    writeStream.on('finish', resolve);
+  });
+
+  await end;
+  console.log('Finished writing to S3 stream');
+};
